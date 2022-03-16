33992a33c2983dd3ff379c19f4eece501700d073
H <hrvojep@yahoo.com>
2022-03-16 03:31:27 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,10 +2,5 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   # insert the 1 required variable here
-  resource_group_name = "h-test"
-}
-
-resource "azurerm_resource_group" "h-test" {
-  name     = "h-test"
-  location = "East US"
+  resource_group_name = "${var.prefix}-workshop"
 }
